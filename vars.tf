c35cdc8d7c60ca72c3c00142ad0037b2b01f5b3e
rifki <rrdvan22@gmail.com>
2023-12-08 19:30:08 +0700

    hapus variables amis

--- vars.tf
+++ vars.tf
@@ -8,12 +8,3 @@ variable "AWS_REGION" {
   default = "ap-southeast-3"
 }
 
-variable "AMIS" {
-  type = map(string)
-  default = {
-    ap-southeast-3 = "ami-0d6c66e2edef56378"
-    ap-southeast-1 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
-  }
-}
-
